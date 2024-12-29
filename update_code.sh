573d1bd7f6ce9817a789b7a5f6a6d07092224239
happyjake <happyjake@gmail.com>
2024-12-29 18:46:04 +0800

    use git resetore

--- update_code.sh
+++ update_code.sh
@@ -1,12 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Get list of tracked .sh files that have shebang changes
-modified_files=$(git status -s | grep "\.sh$" | awk '{print $2}')
+# Store current shebang modifications
+git diff > temp_changes.patch
 
-# Revert Termux shebang to standard in modified files
-for file in $modified_files; do
-    sed -i 's|#!/data/data/com.termux/files/usr/bin/bash|#!/bin/bash|g' "$file"
-done
+# Restore all tracked files to their original state
+git restore .
 
 # Pull changes
 echo "Pulling latest changes..."
