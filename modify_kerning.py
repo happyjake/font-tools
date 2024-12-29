f4b674ca1f4c85305e4deb65ddc19739eca872f4
happyjake <happyjake@gmail.com>
2024-12-29 14:13:11 +0800

    fix unicode

--- modify_kerning.py
+++ modify_kerning.py
@@ -1,16 +1,19 @@
 import sys
 from fontTools.ttLib import TTFont
 
-def get_glyph_name_for_char(font, char):
-    """Get glyph name for a given character"""
-    for table in font['cmap'].tables:
-        if ord(char) in table.cmap:
-            return table.cmap[ord(char)]
+def get_glyph_name_for_unicode(font, unicode_value):
+    """Get glyph name for a unicode value"""
+    try:
+        for table in font['cmap'].tables:
+            if unicode_value in table.cmap:
+                return table.cmap[unicode_value]
+    except TypeError:
+        print(f"警告: 无法处理 Unicode {hex(unicode_value)}")
     return None
 
-def modify_glyph_width(font, char, width_adjustment):
+def modify_glyph_width(font, unicode_value, width_adjustment):
     """Modify the advance width of a glyph"""
-    glyph_name = get_glyph_name_for_char(font, char)
+    glyph_name = get_glyph_name_for_unicode(font, unicode_value)
     if glyph_name and glyph_name in font['hmtx'].metrics:
         advance, lsb = font['hmtx'].metrics[glyph_name]
         font['hmtx'].metrics[glyph_name] = (advance + width_adjustment, lsb)
@@ -25,15 +28,30 @@ def main():
     
     font = TTFont(input_ttf)
     
-    # Target characters to modify
-    quotes = ["'", '"', ''', ''', '"', '"', "‘", "’"]
-    punctuation = [',', '.', ';', ':', '!', '?']
+    # Unicode values for quotes and punctuation
+    quotes = [
+        0x0027,  # ' APOSTROPHE
+        0x0022,  # " QUOTATION MARK
+        0x2018,  # ' LEFT SINGLE QUOTATION MARK
+        0x2019,  # ' RIGHT SINGLE QUOTATION MARK
+        0x201C,  # " LEFT DOUBLE QUOTATION MARK
+        0x201D,  # " RIGHT DOUBLE QUOTATION MARK
+    ]
+    
+    punctuation = [
+        0x002C,  # , COMMA
+        0x002E,  # . FULL STOP
+        0x003B,  # ; SEMICOLON
+        0x003A,  # : COLON
+        0x0021,  # ! EXCLAMATION MARK
+        0x003F,  # ? QUESTION MARK
+    ]
     
     # Adjust width for each character
-    for char in quotes:
-        modify_glyph_width(font, char, -40)  # Reduce width by 40 units
-    for char in punctuation:
-        modify_glyph_width(font, char, -20)  # Reduce width by 20 units
+    for unicode_value in quotes:
+        modify_glyph_width(font, unicode_value, -40)
+    for unicode_value in punctuation:
+        modify_glyph_width(font, unicode_value, -20)
         
     font.save(output_ttf)
     print(f"字体'{input_ttf}'已修改字符宽度，并保存为'{output_ttf}'。")
